fba5e6efee4a362348307e918adb3da0d72c84b6
Sijie-Yang <98970172+Sijie-Yang@users.noreply.github.com>
2025-04-07 15:22:36 +0800

    Update documentation to show functions in urbancode.svi namespace

--- urbancode/__init__.py
+++ urbancode/__init__.py
@@ -2,21 +2,4 @@ __version__ = "0.1.0"
 __author__ = "Sijie Yang"
 __description__ = "A package for universal urban analysis"
 
-# Import svi functions directly into the urbancode namespace
-from urbancode.svi import (
-    filename,
-    color,
-    read_image_with_pil,
-    compute_colorfulness,
-    compute_canny_edges,
-    compute_hue_mean_std,
-    compute_saturation_mean_std,
-    compute_lightness_mean_std,
-    compute_contrast,
-    compute_sharpness,
-    compute_entropy,
-    compute_image_variance,
-    segmentation,
-    object_detection,
-    scene_recognition
-)
+# Remove the direct imports
