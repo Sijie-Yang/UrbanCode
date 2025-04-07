79026e171585f7d7b0dd1c23b0d706ea61520219
Sijie-Yang <98970172+Sijie-Yang@users.noreply.github.com>
2025-04-07 15:35:18 +0800

    Fix module import by explicitly importing svi submodule

--- urbancode/__init__.py
+++ urbancode/__init__.py
@@ -2,4 +2,7 @@ __version__ = "0.1.0"
 __author__ = "Sijie Yang"
 __description__ = "A package for universal urban analysis"
 
+# Import submodules
+import urbancode.svi
+
 # Remove the direct imports
